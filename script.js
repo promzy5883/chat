9ad87e5de5564532e96502374a3c1e33f1133b1e
promise <pebuka351@gmail.com>
2023-01-07 14:25:42 +0100

    C

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 var box = document.getElementById("box1");
 var del = document.getElementById("delete");
 var more = document.getElementById("more");
+var pin = document.getElementById("pin");
+var unread = document.getElementById("unread");
 var message = document.getElementById("message");
+
+window.mouseDown = false;
+document.onmousedown = function () {
+  window.mouseDown = true;
+};
+document.onmouseup = function () {
+  window.mouseDown = false;
+};
 box.addEventListener("mousemove", getMouseDirection, false);
 var oldX = 0;
 
 function getMouseDirection(e) {
-  if (oldX < e.pageX) {
-    xDirection = "right";
+  if (window.mouseDown) {
+    if (oldX < e.pageX) {
+      del.classList.remove("del");
+      more.classList.remove("mor");
+      message.classList.remove("mess");
 
-    del.classList.add("del-al");
-    more.classList.add("mor-al");
-    message.classList.add("mess-al");
-  } else {
-    xDirection = "left";
-    del.classList.add("del");
-    more.classList.add("mor");
-    message.classList.add("mess");
+      pin.classList.add("pi");
+      unread.classList.add("unr");
+      message.classList.add("messL");
+    } else {
+      del.classList.add("del");
+      more.classList.add("mor");
+      message.classList.add("mess");
+
+      pin.classList.remove("pi");
+      unread.classList.remove("unr");
+      message.classList.remove("messL");
+    }
   }
-
   oldX = e.pageX;
 }
