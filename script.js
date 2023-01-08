bd17efd98e1765fb3c767fda9bc40a2fefc6a68d
promise <pebuka351@gmail.com>
2023-01-08 15:59:45 +0100

    C

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var box = document.getElementById("box1");
+/*var box = document.getElementById("box1");
 var del = document.getElementById("delete");
 var more = document.getElementById("more");
 var pin = document.getElementById("pin");
 var unread = document.getElementById("unread");
 var message = document.getElementById("message");
-
+var count = 0;
+var current = 0;
 window.mouseDown = false;
 document.onmousedown = function () {
   window.mouseDown = true;
@@ -12,28 +13,37 @@ document.onmousedown = function () {
 document.onmouseup = function () {
   window.mouseDown = false;
 };
-box.addEventListener("mousemove", getMouseDirection, false);
-var oldX = 0;
 
+box.addEventListener("mousemove", getMouseDirection, false);
+box.addEventListener("mouseover", (e) => {
+  current = e.pageX;
+});
 function getMouseDirection(e) {
   if (window.mouseDown) {
-    if (oldX < e.pageX) {
-      del.classList.remove("del");
-      more.classList.remove("mor");
-      message.classList.remove("mess");
-
-      pin.classList.add("pi");
-      unread.classList.add("unr");
-      message.classList.add("messL");
-    } else {
-      del.classList.add("del");
-      more.classList.add("mor");
-      message.classList.add("mess");
-
-      pin.classList.remove("pi");
-      unread.classList.remove("unr");
-      message.classList.remove("messL");
+    count = e.pageX;
+    if (count < current) {
+      var remainder = current - count;
+      remainder = remainder > 40 ? 40 : remainder;
+      message.style.left = `-${remainder}%`;
+      message.style.transition = "0.5s";
+      more.style.right = `${remainder - 20}%`;
+      more.style.transition = "0.5s";
+      pin.style.left = `-${40}%`;
+      pin.style.transition = "0.5s";
+    }
+    if (count > current) {
+      var remainder = count - current;
+      remainder = remainder > 40 ? 40 : remainder;
+      message.style.left = `${remainder}%`;
+      message.style.transition = "0.5s";
+      more.style.right = `-${40}%`;
+      more.style.transition = "0.5s";
+      pin.style.left = `${remainder - 20}%`;
+      pin.style.transition = "0.5s";
     }
   }
-  oldX = e.pageX;
 }
+*/
+
+var parent = document.getElementById("parent");
+parent.scrollLeft += document.querySelector(".unread").clientWidth;
