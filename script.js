417d2fb0fc0636518f05d1b08321d40d5cc77549
promise <pebuka351@gmail.com>
2023-01-09 19:33:11 +0100

    C

--- script.js
+++ script.js
@@ -1,54 +1,125 @@
-/*var box = document.getElementById("box1");
-var del = document.getElementById("delete");
-var more = document.getElementById("more");
-var pin = document.getElementById("pin");
-var unread = document.getElementById("unread");
-var message = document.getElementById("message");
-var count = 0;
-var current = 0;
-window.mouseDown = false;
-document.onmousedown = function () {
-  window.mouseDown = true;
-};
-document.onmouseup = function () {
-  window.mouseDown = false;
-};
-
-box.addEventListener("mousemove", getMouseDirection, false);
-box.addEventListener("mouseover", (e) => {
-  current = e.pageX;
-});
-function getMouseDirection(e) {
-  if (window.mouseDown) {
-    count = e.pageX;
-    if (count < current) {
-      var remainder = current - count;
-      remainder = remainder > 40 ? 40 : remainder;
-      message.style.left = `-${remainder}%`;
-      message.style.transition = "0.5s";
-      more.style.right = `${remainder - 20}%`;
-      more.style.transition = "0.5s";
-      pin.style.left = `-${40}%`;
-      pin.style.transition = "0.5s";
-    }
-    if (count > current) {
-      var remainder = count - current;
-      remainder = remainder > 40 ? 40 : remainder;
-      message.style.left = `${remainder}%`;
-      message.style.transition = "0.5s";
-      more.style.right = `-${40}%`;
-      more.style.transition = "0.5s";
-      pin.style.left = `${remainder - 20}%`;
-      pin.style.transition = "0.5s";
-    }
-  }
-}
-*/
-
 var parent = document.getElementById("parent");
 parent.scrollLeft += document.querySelector("#unread").clientWidth;
 parent.scrollLeft += document.querySelector("#pin").clientWidth;
+var position;
+window.mouseDown = false;
+document.onmousedown = function (e) {
+  window.mouseDown = true;
+  position = e.pageX + 230;
+  position2 = e.pageX + 230;
+  position3 = e.pageX + 230;
+  position4 = e.pageX + 230;
+};
+document.onmouseup = function () {
+  window.mouseDown = false;
+  if (parent.scrollLeft < 453 && parent.scrollLeft != 0) {
+    parent.scrollLeft = 230;
+  }
+  if (parent2.scrollLeft < 453 && parent2.scrollLeft != 0) {
+    parent2.scrollLeft = 230;
+  }
+  if (parent3.scrollLeft < 453 && parent3.scrollLeft != 0) {
+    parent3.scrollLeft = 230;
+  }
+  if (parent4.scrollLeft < 453 && parent4.scrollLeft != 0) {
+    parent4.scrollLeft = 230;
+  }
+};
+parent.addEventListener("mousemove", (e) => {
+  if (window.mouseDown) {
+    var remainder = position - e.pageX;
+    remainder = remainder > 453 ? 453 : remainder;
+    remainder = remainder < 0 ? 0 : remainder;
+
+    parent.scrollLeft = remainder;
+    parent2.scrollLeft = 230;
+    parent3.scrollLeft = 230;
+    parent4.scrollLeft = 230;
+  }
+});
+
+function delOne() {
+  var box = document.getElementsByClassName("box1");
+  box[0].classList.add("disappear");
+}
 
 var parent2 = document.getElementById("parent2");
 parent2.scrollLeft += document.querySelector("#unread2").clientWidth;
 parent2.scrollLeft += document.querySelector("#pin2").clientWidth;
+var position2;
+parent2.addEventListener("mousemove", (e) => {
+  if (window.mouseDown) {
+    var remainder = position2 - e.pageX;
+    remainder = remainder > 453 ? 453 : remainder;
+    remainder = remainder < 0 ? 0 : remainder;
+
+    parent2.scrollLeft = remainder;
+    parent.scrollLeft = 230;
+    parent3.scrollLeft = 230;
+    parent4.scrollLeft = 230;
+  }
+});
+
+function delTwo() {
+  var box = document.getElementsByClassName("box2");
+  box[0].classList.add("disappear");
+}
+
+var parent3 = document.getElementById("parent3");
+parent3.scrollLeft += document.querySelector("#unread3").clientWidth;
+parent3.scrollLeft += document.querySelector("#pin3").clientWidth;
+var position3;
+parent3.addEventListener("mousemove", (e) => {
+  if (window.mouseDown) {
+    var remainder = position3 - e.pageX;
+    remainder = remainder > 453 ? 453 : remainder;
+    remainder = remainder < 0 ? 0 : remainder;
+
+    parent3.scrollLeft = remainder;
+    parent.scrollLeft = 230;
+    parent2.scrollLeft = 230;
+    parent4.scrollLeft = 230;
+  }
+});
+
+function delThree() {
+  var box = document.getElementsByClassName("box3");
+  box[0].classList.add("disappear");
+}
+
+var parent4 = document.getElementById("parent4");
+parent4.scrollLeft += document.querySelector("#unread4").clientWidth;
+parent4.scrollLeft += document.querySelector("#pin4").clientWidth;
+var position4;
+parent4.addEventListener("mousemove", (e) => {
+  if (window.mouseDown) {
+    var remainder = position4 - e.pageX;
+    remainder = remainder > 453 ? 453 : remainder;
+    remainder = remainder < 0 ? 0 : remainder;
+
+    parent4.scrollLeft = remainder;
+    parent.scrollLeft = 230;
+    parent2.scrollLeft = 230;
+    parent3.scrollLeft = 230;
+  }
+});
+
+function delFour() {
+  var box = document.getElementsByClassName("box4");
+  box[0].classList.add("disappear");
+}
+
+var chatSection = document.getElementById("chat-section");
+var homeSection = document.getElementById("home-section");
+var callSection = document.getElementById("call-section");
+var communitySection = document.getElementById("community-section");
+
+var chatPosition = 0;
+var homePosition = -100;
+var callPosition = -100;
+var communityPosition = 100;
+
+var homeButton = document.getElementById("homeButton");
+var callButton = document.getElementById("callButton");
+var communityButton = document.getElementById("communityButton");
+var chatsButton = document.getElementById("chatsButton");
