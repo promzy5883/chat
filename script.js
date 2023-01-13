bf8d1157c11b4e4121719580f2b09681ee62a3b0
promise <pebuka351@gmail.com>
2023-01-13 21:18:21 +0100

    c

--- script.js
+++ script.js
@@ -59,23 +59,17 @@ parent.addEventListener("touchmove", (e) => {
   if (touchMovement > touchPosition) {
     touchRemainder = touchMovement - touchPosition;
     touchRemainder = touchRemainder > 120 ? 120 : touchRemainder;
-    touchRemainder = touchRemainder < 0 ? 0 : touchRemainder;
     messageBox1.style.transform = `translateX(${touchRemainder}px)`;
     pin1.style.transform = `translateX(${touchRemainder - 60}px)`;
     more1.style.transform = `translateX(${touchRemainder - 60}px)`;
+  } else {
+    touchRemainder = touchPosition - touchMovement;
+    touchRemainder = touchRemainder > 120 ? 120 : touchRemainder;
+    more1.style.transform = `translateX(-${touchRemainder - 60}px)`;
+    messageBox1.style.transform = `translateX(-${touchRemainder}px)`;
+    pin1.style.transform = `translateX(-${touchRemainder - 60}px)`;
   }
-  parent.addEventListener("touchmove", (e) => {
-    touchMovement = e.touches[0].pageX;
-    if (touchMovement < touchPosition) {
-      touchRemainder = touchPosition - touchMovement;
-      touchRemainder = touchRemainder > 120 ? 120 : touchRemainder;
-      touchRemainder = touchRemainder < 0 ? 0 : touchRemainder;
-      touchRemainder = touchRemainder > 0 ? 0 : touchRemainder;
-      more1.style.transform = `translateX(-${touchRemainder - 60}px)`;
-      messageBox1.style.transform = `translateX(-${touchRemainder}px)`;
-      pin1.style.transform = `translateX(-${touchRemainder - 60}px)`;
-    }
-  });
+
   messageBox2.style.transform = `translateX(${0})`;
   pin2.style.transform = `translateX(${-60}px)`;
   more2.style.transform = `translateX(${60}px)`;
